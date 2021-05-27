125ea99bd05be81888762e34163b4e24a077c799
Lacheheb Thomas <thomas.lacheheb@hotmail.fr>
2021-05-27 11:50:32 +0200

    unit tests - contact phone number, tag & update other tests + Update README.md

--- tests/Entity/ContactTest.php
+++ tests/Entity/ContactTest.php
@@ -12,22 +12,37 @@ class ContactTest extends TestCase {
     protected function setUp(): void
     {
         $this->contact = new Contact();
-        $this->contact->setFirstname("John");
-        $this->contact->setLastname("Doe");
-        $this->contact->setEmail("example@mail.com");
-        $this->contact->setPhoneNumber("555788561");
-        $this->contact->setTag("123544");
     }
 
     public function testFirstNameNotEmpty() {
+        $this->assertEmpty($this->contact->getFirstname());
+        $this->contact->setFirstname("John");
         $this->assertNotEmpty($this->contact->getFirstname());
     }
 
     public function testLastNameNotEmpty() {
+        $this->assertEmpty($this->contact->getLastname());
+        $this->contact->setLastname("Doe");
         $this->assertNotEmpty($this->contact->getLastname());
     }
 
     public function testValidEmail() {
+        $this->assertEmpty($this->contact->getEmail());
+        $this->contact->setEmail("example@mail.com");
+        $this->assertNotEmpty($this->contact->getEmail());
         $this->assertSame($this->contact->getEmail(), filter_var($this->contact->getEmail(), FILTER_VALIDATE_EMAIL));
+    }
+
+    public function testPhoneNumber() {
+        $this->assertEmpty($this->contact->getPhoneNumber());
+        $this->contact->setPhoneNumber("0605040302");
+        $this->assertNotEmpty($this->contact->getPhoneNumber());
+        $this->assertGreaterThanOrEqual(10, strlen($this->contact->getPhoneNumber()));
+    }
+
+    public function testTag() {
+        $this->assertEmpty($this->contact->getTag());
+        $this->contact->setTag("123544");
+        $this->assertNotEmpty($this->contact->getTag());
     }
 }
